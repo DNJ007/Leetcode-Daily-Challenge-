fa93d0af273e761a6fa827b10b2ce2b20c3b43c8
DHEEMANTH.N.J. <96421621+DNJ007@users.noreply.github.com>
2024-09-16 13:42:06 +0530

    Add files via upload

---
+++ Strings/_9_16_24_Minimum_Time_Difference.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class _9_16_24_Minimum_Time_Difference
+{
+
+    public static int findMinDifference(List<String> timePoints) 
+    {
+        List<Integer> Al= new ArrayList<>();
+        String[] str = new String[2];
+
+        for(int i=0;i<timePoints.size();i++)
+        {
+            String s = timePoints.get(i);
+            str = s.split(":");
+            if(Integer.parseInt(str[0]) == 00)
+            {
+                str[0] = String.valueOf(24);
+            }
+
+            int min = (Integer.parseInt(str[0]) * 60) + Integer.parseInt(str[1]);
+            Al.add(min);
+        }
+
+        Collections.sort(Al);
+
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<Al.size()-1;i++)
+        {
+            int diff = Math.abs(Al.get(i) - Al.get(i+1));
+
+            min = Math.min(min,diff);
+        }
+        int diff = Math.abs(Al.get(0) - Al.get(Al.size()-1));
+        min = Math.min(min,Math.min(diff,1440-diff));
+        return min;
+    }
+    public static void main(String [] args)
+    {
+        List<String> timePoints = new ArrayList<>();
+        timePoints.add("00:00");
+        timePoints.add("23:59");
+        timePoints.add("00:00");
+        System.out.println(findMinDifference(timePoints)); 
+    }
+}
